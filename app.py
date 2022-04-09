e16adc53ae23a6fb738b46e14929c961847ed893
ADOU1998 <fabriceadou5@gmail.com>
2022-04-09 00:19:33 +0000

    Ajout des fichier bootstrap et message flash

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # Créer une instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
+
+# Création de la class Form
+class NamerForm(FlaskForm):
+    name = StringField("Quel est ton  nom ?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Route de decorateur
 @app.route('/')
@@ -9,7 +19,7 @@ app = Flask(__name__)
 #    return "hello"
 def index():
     first_name = "ADOU"
-    return render_template('index.html', first_name=first_name)
+    return render_template("index.html", first_name=first_name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -25,3 +35,22 @@ def page_not_found(e):
 def page_not_found(e):
      return render_template("500.html"), 500
 
+# route de la page name
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Valider le form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+        #Méssage d'alerte
+        flash('Succès !')
+    return render_template("name.html",
+        name = name,
+        form = form)
+
+# Configuration du serveur
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)
